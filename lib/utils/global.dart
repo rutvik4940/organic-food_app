3ce9d8e975a2a446cfcfbdf6f9a82cf8ed1ee90b
rutvik4940 <153794371+rutvik4940@users.noreply.github.com>
2024-01-05 17:02:37 +0530

    Update global.dart

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -1,15 +1,15 @@
 List <Map> fruitsList=[
-  {"name":"papaya","country":"usa","price":10.00,"image":"assets/images/papaya2.jpg"},
+  {"name":"Papaya","country":"usa","price":10.00,"image":"assets/images/papaya2.jpg"},
   {"name":"Apple","country":"india","price":20.00,"image":"assets/images/apple-fruit.png"},
   {"name":"Sarsaparilla","dubai":"Surat","price":35.00,"image":"assets/images/chiku.jpg"},
   {"name":"Mango","country":"london","price":40.00,"image":"assets/images/mango2.jpg"},
   {"name":"kiwi","country":"england","price":50.00,"image":"assets/images/Kiwi1.jpg"},
 ];
 List <Map> vegtableList=[
-  {"name":"tomato","country":"usa","price":25.00,"image":"assets/images/tomato_281240360_250.jpg"},
-  {"name":"okra","country":"india","price":35.00,"image":"assets/images/bhinda.jpg"},
-  {"name":"onion","country":"dubai","price":30.00,"image":"assets/images/Onion-Red.jpg"},
-  {"name":"garlic","country":" lomdon","price":80.00,"image":"assets/images/garlic.jpg"},
+  {"name":"Tomato","country":"usa","price":25.00,"image":"assets/images/tomato_281240360_250.jpg"},
+  {"name":"Okra","country":"india","price":35.00,"image":"assets/images/bhinda.jpg"},
+  {"name":"Onion","country":"dubai","price":30.00,"image":"assets/images/Onion-Red.jpg"},
+  {"name":"Garlic","country":" lomdon","price":80.00,"image":"assets/images/garlic.jpg"},
   {"name":"Bridal","country":"england","price":70.00,"image":"assets/images/md_Brinjal_Bharta.jpg"},
 ];
 List <Map> cartList=[];
